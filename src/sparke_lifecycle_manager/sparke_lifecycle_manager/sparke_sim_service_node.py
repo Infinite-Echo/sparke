1f51f2b6ab0abd4f2b6f138956340d6927d8723c
Infinite-Echo <colin.fuelberth@icloud.com>
2023-01-01 15:27:57 -0600

    added controllers to sim service node

--- src/sparke_lifecycle_manager/sparke_lifecycle_manager/sparke_sim_service_node.py
+++ src/sparke_lifecycle_manager/sparke_lifecycle_manager/sparke_sim_service_node.py
@@ -1,14 +1,22 @@
+import os
 import sys
 from std_srvs.srv import Empty
 
 import rclpy
 from rclpy.node import Node
 
+from ament_index_python.packages import get_package_share_directory
+from controller_manager_msgs.srv import ReloadControllerLibraries, LoadStartController
 
 class MinimalService(Node):
 
     def __init__(self):
         super().__init__('sparke_simulation_service_node')
+
+        self.reload_req = ReloadControllerLibraries.Request()
+        self.reload_req.force_kill = True
+        self.load_req = LoadStartController.Request()
+
         self.srv = self.create_service(Empty, 'reset_sparke_sim', self.reset_sparke_sim_callback)
         self.init_clients()
         
@@ -27,6 +35,14 @@ class MinimalService(Node):
         while not self.unpause_cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('service not available, waiting again...')
         
+        self.reload_controllers_cli = self.create_client(ReloadControllerLibraries, "/controller_manager/reload_controller_libraries")
+        while not self.reload_controllers_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+
+        self.load_controllers_cli = self.create_client(LoadStartController, "/controller_manager/load_and_start_controller")
+        while not self.load_controllers_cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...')
+        
     def pause_gazebo(self):
         self.get_logger().info('Pausing Gazebo...')
         self.pause_cli.call_async(self.req)
@@ -39,9 +55,22 @@ class MinimalService(Node):
         self.get_logger().info('Unpausing Gazebo...')
         self.unpause_cli.call_async(self.req)
 
+    def reload_controllers(self):
+        self.get_logger().info('Reloading Controllers...')
+        self.reload_controllers_cli.call_async(self.reload_req)
+
+    def load_controllers(self):
+        self.get_logger().info('Restarting Controllers...')
+        self.load_req.name = "joint_states_controller"
+        self.load_controllers_cli.call_async(self.load_req)
+        self.load_req.name = "joint_group_effort_controller"
+        self.load_controllers_cli.call_async(self.load_req)
+
     def reset_sparke_sim_callback(self, request, response):
         self.pause_gazebo()
+        self.reload_controllers()
         self.reset_gazebo()
+        self.load_controllers()
         self.unpause_gazebo()
         self.get_logger().info("this worked")
         return Empty.Response()
