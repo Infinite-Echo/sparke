e50e1388a100f6f934c884e89bee41af01603462
Infinite-Echo <colin.fuelberth@icloud.com>
2023-01-04 15:42:50 -0600

    node now works as intended

--- src/sparke_lifecycle_manager/sparke_lifecycle_manager/sparke_reset_manager.py
+++ src/sparke_lifecycle_manager/sparke_lifecycle_manager/sparke_reset_manager.py
@@ -6,70 +6,83 @@ from rclpy.node import Node
 from std_srvs.srv import Empty
 from controller_manager_msgs.srv import SwitchController
 
-def service_ready_check(node, client, executor):
-    while not client.service_is_ready():
-        node.get_logger().info("Waiting for service...")
-        executor.spin_once(timeout_sec=0.1)
+class sparke_manager(Node):
+    def __init__(self):
+        super().__init__("sparke_reset_manager", allow_undeclared_parameters=True, automatically_declare_parameters_from_overrides=True)
+        client_cb_group = MutuallyExclusiveCallbackGroup()
+        service_cb_group = MutuallyExclusiveCallbackGroup()
 
-def future_check(node, future_obj):
-    if future_obj.done():
-        node.get_logger().info(f"Future Result: {future_obj.result()}")
-    else: 
-        node.get_logger().info("Future never finished.")
+        #create service
+        self.reset_sparke_service = self.create_service(Empty, "/reset_sparke", self.reset_sparke_cb, callback_group=service_cb_group)
+
+        #create clients
+        self.switch_controller_client = self.create_client(SwitchController, "/controller_manager/switch_controller", callback_group=client_cb_group)
+        self.reset_gazebo_client = self.create_client(Empty, "/reset_simulation", callback_group=client_cb_group)
+
+        self.stop_controllers_req = SwitchController.Request()
+        self.start_controllers_req = SwitchController.Request()
+        self.empty_req = Empty.Request()
+        self.init_msgs()
+        self.reset_called = False
+
+    def reset_sparke_cb(self, req, res):
+        #Stop Controllers
+        self.service_ready_check(self.switch_controller_client)
+        stop_future = self.switch_controller_client.call_async(self.stop_controllers_req)
+        while not stop_future.done():
+            self.get_logger().info("Waiting on Future 1...")
+        self.future_check(stop_future)
+        self.get_logger().info("Controllers Stopped Successfully.")
+
+        #Reset Gazebo
+        self.service_ready_check(self.reset_gazebo_client)
+        reset_future = self.reset_gazebo_client.call_async(self.empty_req)
+        while not reset_future.done():
+            self.get_logger().info("Waiting on Future 2...")
+        self.future_check(reset_future)
+        self.get_logger().info("Gazebo Reset Successfully.")
+
+        #Start Controllers
+        self.service_ready_check(self.switch_controller_client)
+        start_future = self.switch_controller_client.call_async(self.start_controllers_req)
+        while not start_future.done():
+            self.get_logger().info("Waiting on Future 3...")
+        self.future_check(start_future)
+        self.get_logger().info("Controller Started Successfully.")
+
+        #Confirm Program Reached This Point
+        self.get_logger().info("If you see this then the future result should have been printed already.")
+        res = Empty.Response()
+        return res
+
+    def service_ready_check(self, client):
+        while not client.service_is_ready():
+            self.get_logger().info("Waiting for service...")
+
+    def future_check(self, future_obj):
+        if future_obj.done():
+            self.get_logger().info(f"Future Result: {future_obj.result()}")
+        else: 
+            self.get_logger().info("Future never finished.")
+
+    def init_msgs(self):
+        self.stop_controllers_req.stop_controllers = ["joint_states_controller", "joint_group_effort_controller"]
+        self.stop_controllers_req.strictness = 1
+        self.start_controllers_req.start_controllers = ["joint_states_controller", "joint_group_effort_controller"]
+        self.start_controllers_req.strictness = 1
 
 def main(args=None):
     rclpy.init(args=args)
-    client_node = rclpy.create_node("sparke_reset_manager", allow_undeclared_parameters=True, automatically_declare_parameters_from_overrides=True)
-    client_cb_group = None
 
-    #create clients
-    switch_client = client_node.create_client(SwitchController, "/controller_manager/switch_controller", callback_group=client_cb_group)
-    reset_client = client_node.create_client(Empty, "/reset_simulation", callback_group=client_cb_group)
-
-    loop_count = 0
-
-    #create request messages
-    stop_controllers_req = SwitchController.Request()
-    stop_controllers_req.stop_controllers = ["joint_states_controller", "joint_group_effort_controller"]
-    stop_controllers_req.strictness = 1
-    start_controllers_req = SwitchController.Request()
-    start_controllers_req.start_controllers = ["joint_states_controller", "joint_group_effort_controller"]
-    start_controllers_req.strictness = 1
-    empty_req = Empty.Request()
+    manager_node = sparke_manager()
 
     executor = MultiThreadedExecutor()
-    executor.add_node(client_node)
+    executor.add_node(manager_node)
 
     while rclpy.ok():
-        if not loop_count >= 1:
-            #Stop Controllers
-            service_ready_check(client_node, switch_client, executor)
-            stop_future = switch_client.call_async(stop_controllers_req)
-            while not stop_future.done():
-                executor.spin_once(timeout_sec=1.0)
-            future_check(client_node, stop_future)
-            client_node.get_logger().info("Controllers Stopped Successfully.")
+        executor.spin()
 
-            #Pause Gazebo
-            service_ready_check(client_node, reset_client, executor)
-            reset_future = reset_client.call_async(empty_req)
-            while not reset_future.done():
-                executor.spin_once(timeout_sec=1.0)
-            future_check(client_node, reset_future)
-            client_node.get_logger().info("Gazebo Reset Successfully.")
-
-            #Start Controllers
-            service_ready_check(client_node, switch_client, executor)
-            start_future = switch_client.call_async(start_controllers_req)
-            while not start_future.done():
-                executor.spin_once(timeout_sec=1.0)
-            future_check(client_node, start_future)
-            client_node.get_logger().info("Controllers Started Successfully.")
-
-            #Confirm Program Reached This Point
-            client_node.get_logger().info("If you see this then the future result should have been printed already.")
-            loop_count += 1
-
+    executor.shutdown()
     rclpy.shutdown()
 
 if __name__ == '__main__':
